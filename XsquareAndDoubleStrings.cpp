89050016d038b6fe001f179382fac5d513d547a7
Sujosh Nag <116555835+nagsujosh@users.noreply.github.com>
2023-04-22 14:58:05 +0600

    Update XsquareAndDoubleStrings.cpp

--- XsquareAndDoubleStrings.cpp
+++ XsquareAndDoubleStrings.cpp
@@ -28,3 +28,7 @@ int main()
         else cout << "No\n";
     }
 }
+
+/*
+Question:https://www.hackerearth.com/practice/data-structures/hash-tables/basics-of-hash-tables/practice-problems/algorithm/xsquare-and-double-strings-1/
+*/
